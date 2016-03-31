106bcb72ffa25fd3610f660488f0713032d30a81
trainorpj <trainorpj@gmail.com>
2016-03-31 08:37:57 -0400

    switch from list to dictionary

--- tools/databuild.py
+++ tools/databuild.py
@@ -1,6 +1,5 @@
 import os, re, nltk, json, sklearn
 from freqmetrics import *
-from nltk import stopwords
 
 #lengths of warde, barch, docto, framl, smallh, lastc
 chapter_lengths = [21,53,47,48,60,84]
@@ -9,8 +8,7 @@ books = ["warde", "barch", "docto", "framl","smallh","lastc"]
 book_titles=["Warden", "Barchester_Towers","Doctor_Thorne",
 	"Framley_Parsonage","Small_House", "Last_Chronicle"]
 	
-chapter_titles = []
-
+chapter_titles = {}
 
 master_dict = []
 
@@ -32,14 +30,21 @@ def addChap(title, value, book):
 	new = {'chap': title, 'val': value}
 	book.append(new)
 	
-def makeTitles(path_in, name):
+def makeTitles(path_in, name, realtitle):
 	#Makes list of chapter titles
-	new=[]
+	# puts them into a dictionary
+	index=1
+	chapter_titles[name]={}
+	chapter_titles[name]["chapters"]={}
+	chapter_titles[name]["trunc"]=name
+	chapter_titles[name]["realtitle"]=realtitle
 	with open(path_in, "r") as infile:
 		titles = json.load(infile)
 	for line in titles[name]:
-		new.append(line)
-	chapter_titles.append(new)
+		chapter_titles[name]["chapters"][line] = {}
+		chapter_titles[name]["chapters"][line]["index"] = index
+		index = index +1
+	
 	
 def numZero(num):
 	#Determines if end of file ends as
@@ -49,38 +54,78 @@ def numZero(num):
 	else:
 		return "0"+str(num)
 		
-def readChap(path_in, corp):
-	#Reads in the chapter, returns a 
-	# value to measure frequency---see
-	# freqmetrics.py for possible values
+def readChap(path_in, title, chap):
+	#Reads in the chapter, puts 
+	# list of tokens in main dictionary
 	with open(path_in, "r") as infile:
 		text = infile.read().decode("utf-8")
 		tokens = nltk.word_tokenize(text)
 		#MAKE LOWER CASE
 		#tokens = tokenize[w.lower() for w in tokenize]
 		#return corpFreq(tokens,corp)
-		return chapFreq(tokens, corp)
-	
-def genData(tpc,i):
-	addTopic(tpc)
-	#goes through each book, sets up paths
-	for b in range(len(books)):
+		chapter_titles[title]["chapters"][chap]["tokens"]=tokens
+
+def makeBookLists():
+	for b in range(len(books)):	
 		path0 = "../chap-" + books[b]
 		book_path = path0 +"/Trollope_" + book_titles[b] + "_text_"
 		print book_path
-		makeTitles(path0+"/chap-titles.json", books[b])
-		addInfo(book_titles[b],chapter_lengths[b],master_dict[i]['books'])
-		#runs through each chapter
-		for c in range(chapter_lengths[b]):
-			chap_path = book_path + numZero(c+1)+".txt"
-			value = readChap(chap_path,allcorps[tpc])
-			name = chapter_titles[b][c]
-			addChap(name,value,master_dict[i]['books'][b]['b'])
+		makeTitles(path0+"/chap-titles.json", books[b], book_titles[b])
+	#	for c in range(chapter_lengths[b]):
+	#		chap_path = book_path + numZero(c+1)+".txt"
+	#		readChap(chap_path,books[b])
+	for title in chapter_titles:
+		path0 = "../chap-" + chapter_titles[title]["trunc"]
+		book_path = path0 + "/Trollope_" + chapter_titles[title]["realtitle"] + "_text_"
+		for c in chapter_titles[title]["chapters"]:
+			num = chapter_titles[title]["chapters"][c]["index"]
+			chap_path = book_path + numZero(num) + ".txt"
+			readChap(chap_path, title, c)
+			
 
+def genData(topic,j):
+	addTopic(topic)
+	#for title in chapter_titles:
+	for k in range(len(books)):
+		for titlesrch in chapter_titles:
+			if chapter_titles[titlesrch]['trunc'] == books[k]:
+				title = titlesrch
+		bookname = chapter_titles[title]['realtitle']
+		num = len(chapter_titles[title]['chapters'])
+		addInfo(bookname,num, master_dict[j]['books'])
+		for i in range(num):
+			ind = i+1
+			#search for index 
+			for dum in chapter_titles[title]['chapters']:
+				if chapter_titles[title]['chapters'][dum]['index'] == ind:
+					#print chapter_titles[title]['chapters'][dum]['index']
+					chaptertok = dum
+			#search for index in master_dict
+			mast_index=0
+			for srch in master_dict[j]['books']:
+				if srch['title'] == bookname:
+					break
+				else:
+					mast_index=mast_index+1
+			#print mast_index	
+			toklst = chapter_titles[title]['chapters'][chaptertok]['tokens']
+			value = chapFreq(toklst, allcorps[topic])
+				
+			addChap(chaptertok, value, master_dict[j]['books'][mast_index]['b'])
+			
+	
+		
+makeBookLists()
 for i in range(len(allcorps)):			
 	genData(allcorps.keys()[i],i)
 
 
+#print master_dict
+
+#for i in master_dict[0]['books'][0]['b']:
+#	print i
+
+
 with open("../vis-data/data.json","w") as outfile:
-	dum = json.dumps(master_dict)
-	outfile.write(dum)
+	dummy = json.dumps(master_dict)
+	outfile.write(dummy)
--- tools/freqmetrics.py
+++ tools/freqmetrics.py
@@ -34,13 +34,16 @@ allcorps = {
 		"mamma","gentleman","lady","wife","husband","children","family",
 		"girl","boy","marry","married","marriage","child"
 		],
-	"class": ['archdeacon', 'bishop', 'dr', 'friend', 'old', 'wife', 
+	"class_and_profession": ['archdeacon', 'bishop', 'dr', 'friend', 'old', 'wife', 
 		'church', 'poor', 'income', 'doctor', 'woman', 'men', 'money', 'dean', 'lord', 'squire', 'family', 'lady', 'public', 'pounds', 'husband', 'rate', 'brother', 'clergyman', 'precentor', 'st', 'alone', 'declared', 'duty', 'palace', 'london', 'power', 'girl', 'carriage', 'gentleman', 'widow', 'lordship', 'children', 'ladies', 'married', 'attorney-general', 'appointment', 'opinion', 'lawyer', 'property', 'business', 'justice', 'service', 'oxford', 'pay', 'england', 'rich', 'bedesmen', 'wardenship'
 		],
-	"abstract": ['know', 'think', 'never', 'thought', 'nothing', 'time',
+	"abstract_concepts": ['know', 'think', 'never', 'thought', 'nothing', 'time',
 		'love', 'mind', 'world', 'year', 'heart', 'always', 'moment', 'life', 'money', 'present', 'place', 'name', 'soon', 'poor', 'wish', 'public', 'home', 'till', 'doubt', 'hope', 'happy', 'together', 'things', 'anything', 'morning', 'best', 'understand', 'idea', 'truth', 'while', 'known', 'quiet', 'duty', 'comfort', 'power', 'hour', 'thinking', 'feeling', 'days', 'age', 'evening', 'impossible', 'live', 'opinion', 'care', 'possible', 'hard', 'law', 'tone', 'order', 'conduct', 'occasion', 'circumstances', 'fact', 'loved', 'feelings', 'nature', 'evil', 'justice', 'character', 'strong', 'charity', 'believe', 'promise', 'angry', 'rich', 'sorrow'
 		],
 	"conflict": ['no', "n't", 'can', 'say', 'never', 'though', 'other', 'thought', 'nothing', 'too',
 		'without', 'only', 'however', 'also', 'felt', 'ever', 'What', 'new', 'yet', 'against', 'another', 'nor', 'why', 'question', 'enough', 'better', 'wrong', 'thus', 'whether', 'certainly', 'understand', 'idea', 'petition', 'truth', 'answer', 'declared', 'power', 'angry', 'Bold', 'ask', 'gone', 'end', 'continued', 'known', 'know', 'bear', 'impossible', 'duty', 'knows', 'opinion', 'care', 'possible', 'lawyer', 'law', 'probably', 'nearly', 'order', 'conduct', 'feelings', 'advice', 'allow', 'evil', 'justice', 'angry', 'duties', 'fear'
+		],
+	"domestic": ['Mr', 'man', 'Mrs', 'himself', 'Lady',
+		'father', 'Miss', 'Sir', 'herself', 'friend', 'house', 'love', 'wife', 'Mr', 'man', 'Mrs', 'himself', 'Lady', 'father', 'Miss', 'Sir', 'herself', 'friend', 'house', 'love', 'wife', 'Mr', 'Mr', 'man', 'Mrs', 'himself', 'Lady', 'father', 'Miss', 'Sir', 'herself', 'friend', 'house', 'love', 'wife', 'Mr', 'house', 'room', 'poor', 'husband', 'son-in-law', 'home', 'family', 'quiet', 'comfort', 'palace', 'party', 'carriage', 'door', 'brother', 'wife', 'mamma', 'table', 'sister', 'mother', 'dinner', 'garden', 'daily'
 		]
 	}
