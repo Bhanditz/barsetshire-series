2f9003f19d255314abd2cb73865798340d533c5c
Patrick (PJ) Trainor <trainorpj@gmail.com>
2016-10-24 21:32:01 -0400

    Fixed titles

--- data/info.js
+++ data/info.js
@@ -2,7 +2,7 @@ var titleinfo = [
 	{"name":"The Warden", "numchap":21, "xval":53.0},
 	{"name":"Barchester Towers","numchap":53, "xval":160},
 	{"name":"Doctor Thorne","numchap":47, "xval":346},
-	{"name":"Framely Parsonage","numchap":48, "xval":496},
-	{"name":"Small House at Allington","numchap":60, "xval":659},
+	{"name":"Framley Parsonage","numchap":48, "xval":496},
+	{"name":"The Small House at Allington","numchap":60, "xval":659},
 	{"name":"The Last Chronicle of Barset","numchap":84, "xval":895},
 	]
